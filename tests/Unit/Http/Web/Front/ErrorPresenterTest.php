974824e5e351987b490213676f16c4ce3914866d
Petr Simecek <peter.simecek87@gmail.com>
2025-02-28 20:49:37 +0100

    test: add basic presenter tests

---
+++ tests/Unit/Http/Web/Front/ErrorPresenterTest.php
@@ -0,0 +1,73 @@
+<?php
+
+declare(strict_types=1);
+
+namespace VsPoint\Test\Unit\Http\Web\Front;
+
+use Nette\Application\BadRequestException;
+use Nette\Application\IPresenterFactory;
+use Nette\Application\Request;
+use Nette\Application\Responses\TextResponse;
+use Nette\Bridges\ApplicationLatte\Template;
+use VsPoint\Http\Web\Front\ErrorPresenter;
+use VsPoint\Test\TestCase;
+
+/**
+ * @covers \VsPoint\Http\Web\Front\ErrorPresenter
+ */
+final class ErrorPresenterTest extends TestCase
+{
+  /**
+   * @group unit
+   */
+  public function testBadRequestConstructor(): void
+  {
+    $container = $this->createContainerForWeb();
+
+    $presenterFactory = $container->getByType(IPresenterFactory::class);
+
+    $presenterName = trim(ErrorPresenter::LINK, ':');
+
+    $presenter = $presenterFactory->createPresenter($presenterName);
+
+    self::assertInstanceOf(ErrorPresenter::class, $presenter);
+
+    $presenter->autoCanonicalize = false;
+
+    $request = new Request($presenterName, 'GET', [
+      'action' => 'default',
+      'exception' => new BadRequestException('Bad request', 404),
+    ]);
+    $response = $presenter->run($request);
+
+    self::assertInstanceOf(TextResponse::class, $response);
+    self::assertInstanceOf(Template::class, $response->getSource());
+  }
+
+  /**
+   * @group unit
+   */
+  public function testExceptionConstructor(): void
+  {
+    $container = $this->createContainerForWeb();
+
+    $presenterFactory = $container->getByType(IPresenterFactory::class);
+
+    $presenterName = trim(ErrorPresenter::LINK, ':');
+
+    $presenter = $presenterFactory->createPresenter($presenterName);
+
+    self::assertInstanceOf(ErrorPresenter::class, $presenter);
+
+    $presenter->autoCanonicalize = false;
+
+    $request = new Request($presenterName, 'GET', [
+      'action' => 'default',
+      'exception' => new \Exception('New exception', 500),
+    ]);
+    $response = $presenter->run($request);
+
+    self::assertInstanceOf(TextResponse::class, $response);
+    self::assertInstanceOf(Template::class, $response->getSource());
+  }
+}
---
+++ tests/Unit/Http/Web/Front/HomepagePresenterTest.php
@@ -0,0 +1,44 @@
+<?php
+
+declare(strict_types=1);
+
+namespace VsPoint\Test\Unit\Http\Web\Front;
+
+use Nette\Application\IPresenterFactory;
+use Nette\Application\Request;
+use Nette\Application\Responses\TextResponse;
+use Nette\Bridges\ApplicationLatte\Template;
+use VsPoint\Http\Web\Front\HomepagePresenter;
+use VsPoint\Test\TestCase;
+
+/**
+ * @covers \VsPoint\Http\Web\Front\HomepagePresenter
+ */
+final class HomepagePresenterTest extends TestCase
+{
+  /**
+   * @group unit
+   */
+  public function testConstructor(): void
+  {
+    $container = $this->createContainerForWeb();
+
+    $presenterFactory = $container->getByType(IPresenterFactory::class);
+
+    $presenterName = trim(HomepagePresenter::LINK, ':');
+
+    $presenter = $presenterFactory->createPresenter($presenterName);
+
+    self::assertInstanceOf(HomepagePresenter::class, $presenter);
+
+    $presenter->autoCanonicalize = false;
+
+    $request = new Request($presenterName, 'GET', [
+      'action' => 'default',
+    ]);
+    $response = $presenter->run($request);
+
+    self::assertInstanceOf(TextResponse::class, $response);
+    self::assertInstanceOf(Template::class, $response->getSource());
+  }
+}
